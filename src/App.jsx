a0d6c49257dec2a2c2c8dd2a9a8a26b04a858483
Gbenga <gbengaoluwadahunsi01@gmail.com>
2024-01-07 21:36:38 +0800

    oh

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Content from './components/Content'
 import Footer from './components/Footer'
 import Navbar  from './components/Navbar'
 // import Notification from './components/Notification'
-import ScrollUp from './components/ScrollUp'
+import ScrollUp from './components/ScrollUp';
 
 
 
 const App = () => {
+  const hommie={
+    background: "linear-gradient(to bottom , #581b1be6, rgba(223, 43, 85, 0.2)), url('/Images/test.png')"
+  }
   
   
   return  (
     <div className='container'>
         <div className='app-body'>
           <Navbar/>
-          <main className='homepage'>
+          <main className='homepage' style={hommie}>
             <div className='countria-section'>
               <h1>Countria</h1>
               <p>Countries information,current weather forecast in a fast and exciting manner</p>
